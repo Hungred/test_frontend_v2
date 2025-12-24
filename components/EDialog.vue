7c640295d0b43f35e33d25d50648d2a7932af4ab
Anita <anita397892@gamil.com>
2025-12-24 14:28:14 +0800

    modify: 修改dialog

--- components/EDialog.vue
+++ components/EDialog.vue
@@ -1,10 +1,17 @@
 <template>
   <dialog ref="dialogRef" class="e-dialog">
-    <div class="e-dialog__content">
-      <h3 class="text-lg mb-4">{{ title || $t('dialogTitle.add') }}</h3>
-      <div class="flex justify-end gap-3">
-        <EBtn color="success" @click="$emit('confirm')">{{ $t('confirm') }}</EBtn>
-        <EBtn color="error" @click="close">{{ $t('cancel') }}</EBtn>
+    <div class="e-dialog__content flex flex-col h-full">
+      <div class="text-lg mb-4 text-center">{{ title || $t('dialogTitle.add') }}</div>
+      <div class="bg-black/40 p-4 rounded-md mb-4 flex-1 overflow-auto">
+        <div class="text-md my-2">{{ $t('dialogContent') }}</div>
+      </div>
+      <div class="flex justify-center gap-4 flex-wrap">
+        <EBtn color="success" class="flex-1 min-w-[100px]" @click="$emit('confirm')">
+          {{ $t('confirm') }}
+        </EBtn>
+        <EBtn color="error" class="flex-1 min-w-[100px]" @click="close">
+          {{ $t('cancel') }}
+        </EBtn>
       </div>
     </div>
   </dialog>
@@ -30,8 +37,24 @@ defineExpose({ show, close })
   padding: 1.5rem;
   border-radius: 12px;
   border: 1px solid #555;
+  width: min(70%, 400px);
+  max-width: 400px;
+  min-width: 270px;
+  max-height: 90vh;
   &::backdrop {
     background: rgba(0, 0, 0, 0.7);
+  }
+}
+
+@media (max-width: 320px) {
+  .e-dialog {
+    padding: 1rem;
+    font-size: 14px;
+  }
+
+  .e-dialog__content button {
+    font-size: 12px;
+    padding: 6px 12px;
   }
 }
 </style>
